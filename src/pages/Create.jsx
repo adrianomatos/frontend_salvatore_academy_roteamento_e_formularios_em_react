438e0fa73af54cbe7311dd86ed66711f7e0f03b3
Adriano Matos <adrianotaua@hotmail.com>
2024-06-09 23:08:40 -0300

    Criação do formulário

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,3 +1,41 @@
-export default function Create(){
-	return <div>Create</div>
+export default function Create() {
+  return (
+    <div>
+      <h1>Criar Devmon</h1>
+			
+      <form>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            name="nome"
+            id="nome"
+            placeholder="Digite seu nome"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="imagem">URL da imagem</label>
+          <input
+            type="text"
+            name="imagem"
+            id="imagem"
+            placeholder="Digite a URL da imagem"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="categoria">Categoria (opcional)</label>
+          <input
+            type="text"
+            name="categoria"
+            id="categoria"
+            placeholder="Digite a categoria"
+          />
+        </div>
+
+				<div><input type="submit" value="Cadastrar" /></div>
+      </form>
+    </div>
+  );
 }
